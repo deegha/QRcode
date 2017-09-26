d4897c7a120196f0bfd75433fd892d1bbf3e734f
deegha <shuboothi@gmail.com>
2017-09-27 01:44:54 +0530

    removed unwanted comments

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -11,10 +11,6 @@ export class ListPage {
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 
@@ -29,7 +25,6 @@ export class ListPage {
   }
 
   itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
     this.navCtrl.push(ListPage, {
       item: item
     });
